70099004973c1c8ae64b30cdf17ad08b54f07934
Eva <yi.zheng1709@gmail.com>
2021-01-20 22:54:11 -0500

    commit tree be8074d08f182813aa2e7e7d4027b5297807296f

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -6,8 +7,59 @@ class Song
     @@all
   end
 
-  def save
-    self.class.all << self
+  def save #instance method 
+    self.class.all << self #Song.all << instance_of_song 
   end
+
+  def self.create #class method 
+    song = self.new 
+    self.all << song 
+    song 
+  end 
+
+  def self.new_by_name(name) #input of a string as name of song
+    song = self.new
+    song.name = name 
+    song 
+  end 
+
+  def self.create_by_name(name) #takes string name; returns song instance 
+    song = self.new_by_name(name)
+    song.save  
+    song 
+  end 
+
+  def self.find_by_name(name) #takes string name; returns the matching instance
+    self.all.find {|song| song.name == name} #find the name property
+  end 
+
+  def self.find_or_create_by_name(name) #accept string name
+    if !self.find_by_name(name)
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
+    end 
+  end  
+      
+  def self.alphabetical 
+    self.all.sort_by {|song| song.name}
+  end 
+
+  def self.new_from_filename(filename) # "Taylor Swift - Blank Space.mp3"
+    file = filename.split(" - ")
+    song = self.new 
+    song.artist_name = file[0]
+    song.name = file[1].delete_suffix(".mp3")
+    song 
+  end 
+
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    self.all << song 
+  end 
+
+  def self.destroy_all
+    self.all.clear 
+  end 
 
 end
